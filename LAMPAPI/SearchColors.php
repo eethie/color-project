431141481d7a1d081b90c323ece67eea7ac5f664
Ethan <ethan.z.zeng@gmail.com>
2026-09-10 22:08:39 -0400

    Colors page

--- LAMPAPI/SearchColors.php
+++ LAMPAPI/SearchColors.php
@@ -1,6 +1,19 @@
-$conn = new mysqli(
-    "localhost",
-    "TheBeast",
-    "WeLoveCOP4331",
-    "COP4331"
-);
+<?php
+
+$host = "localhost";
+$user = "YOUR_DB_USER";
+$password = "YOUR_DB_PASSWORD";
+$database = "ContactManager";
+
+$conn = new mysqli($host, $user, $password, $database);
+
+if ($conn->connect_error) {
+    http_response_code(500);
+
+    echo json_encode([
+        "error" => "Database connection failed"
+    ]);
+
+    exit();
+}
+?>
