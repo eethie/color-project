4d7e3ec2794708ef7d098c516908fcdbfa5b366b
Ethan <ethan.z.zeng@gmail.com>
2026-09-17 18:00:15 -0400

    password hashing added

---
+++ js/md5.js
@@ -0,0 +1,120 @@
+// Minimal MD5 implementation for client-side password hashing before sending to the API.
+function md5(inputString) {
+  function rotateLeft(n, s) { return (n << s) | (n >>> (32 - s)); }
+  function toUnsigned(n) { return n >>> 0; }
+
+  function addUnsigned(x, y) {
+    var x8 = x & 0x80000000, y8 = y & 0x80000000;
+    var x4 = x & 0x40000000, y4 = y & 0x40000000;
+    var result = (x & 0x3FFFFFFF) + (y & 0x3FFFFFFF);
+    if (x4 & y4) return result ^ 0x80000000 ^ x8 ^ y8;
+    if (x4 | y4) {
+      if (result & 0x40000000) return result ^ 0xC0000000 ^ x8 ^ y8;
+      return result ^ 0x40000000 ^ x8 ^ y8;
+    }
+    return result ^ x8 ^ y8;
+  }
+
+  function F(x, y, z) { return (x & y) | (~x & z); }
+  function G(x, y, z) { return (x & z) | (y & ~z); }
+  function H(x, y, z) { return x ^ y ^ z; }
+  function I(x, y, z) { return y ^ (x | (~z)); }
+
+  function FF(a, b, c, d, x, s, ac) {
+    a = addUnsigned(a, addUnsigned(addUnsigned(F(b, c, d), x), ac));
+    return addUnsigned(rotateLeft(a, s), b);
+  }
+  function GG(a, b, c, d, x, s, ac) {
+    a = addUnsigned(a, addUnsigned(addUnsigned(G(b, c, d), x), ac));
+    return addUnsigned(rotateLeft(a, s), b);
+  }
+  function HH(a, b, c, d, x, s, ac) {
+    a = addUnsigned(a, addUnsigned(addUnsigned(H(b, c, d), x), ac));
+    return addUnsigned(rotateLeft(a, s), b);
+  }
+  function II(a, b, c, d, x, s, ac) {
+    a = addUnsigned(a, addUnsigned(addUnsigned(I(b, c, d), x), ac));
+    return addUnsigned(rotateLeft(a, s), b);
+  }
+
+  function convertToWordArray(str) {
+    var wordCount;
+    var messageLength = str.length;
+    var numberOfWordsTemp1 = messageLength + 8;
+    var numberOfWordsTemp2 = (numberOfWordsTemp1 - (numberOfWordsTemp1 % 64)) / 64;
+    var numberOfWords = (numberOfWordsTemp2 + 1) * 16;
+    var wordArray = new Array(numberOfWords - 1);
+    var bytePosition = 0, byteCount = 0;
+    while (byteCount < messageLength) {
+      wordCount = (byteCount - (byteCount % 4)) / 4;
+      bytePosition = (byteCount % 4) * 8;
+      wordArray[wordCount] = wordArray[wordCount] | (str.charCodeAt(byteCount) << bytePosition);
+      byteCount++;
+    }
+    wordCount = (byteCount - (byteCount % 4)) / 4;
+    bytePosition = (byteCount % 4) * 8;
+    wordArray[wordCount] = wordArray[wordCount] | (0x80 << bytePosition);
+    wordArray[numberOfWords - 2] = messageLength << 3;
+    wordArray[numberOfWords - 1] = messageLength >>> 29;
+    return wordArray;
+  }
+
+  function toHex(num) {
+    var hexStr = "", val, count;
+    for (count = 0; count <= 3; count++) {
+      val = (num >>> (count * 8)) & 255;
+      hexStr += ("0" + val.toString(16)).slice(-2);
+    }
+    return hexStr;
+  }
+
+  var x = convertToWordArray(unescape(encodeURIComponent(inputString)));
+  var a = 0x67452301, b = 0xEFCDAB89, c = 0x98BADCFE, d = 0x10325476;
+
+  var S11=7,S12=12,S13=17,S14=22,S21=5,S22=9,S23=14,S24=20;
+  var S31=4,S32=11,S33=16,S34=23,S41=6,S42=10,S43=15,S44=21;
+
+  for (var k = 0; k < x.length; k += 16) {
+    var AA=a, BB=b, CC=c, DD=d;
+
+    a=FF(a,b,c,d,x[k+0],S11,0xd76aa478); d=FF(d,a,b,c,x[k+1],S12,0xe8c7b756);
+    c=FF(c,d,a,b,x[k+2],S13,0x242070db); b=FF(b,c,d,a,x[k+3],S14,0xc1bdceee);
+    a=FF(a,b,c,d,x[k+4],S11,0xf57c0faf); d=FF(d,a,b,c,x[k+5],S12,0x4787c62a);
+    c=FF(c,d,a,b,x[k+6],S13,0xa8304613); b=FF(b,c,d,a,x[k+7],S14,0xfd469501);
+    a=FF(a,b,c,d,x[k+8],S11,0x698098d8); d=FF(d,a,b,c,x[k+9],S12,0x8b44f7af);
+    c=FF(c,d,a,b,x[k+10],S13,0xffff5bb1); b=FF(b,c,d,a,x[k+11],S14,0x895cd7be);
+    a=FF(a,b,c,d,x[k+12],S11,0x6b901122); d=FF(d,a,b,c,x[k+13],S12,0xfd987193);
+    c=FF(c,d,a,b,x[k+14],S13,0xa679438e); b=FF(b,c,d,a,x[k+15],S14,0x49b40821);
+
+    a=GG(a,b,c,d,x[k+1],S21,0xf61e2562); d=GG(d,a,b,c,x[k+6],S22,0xc040b340);
+    c=GG(c,d,a,b,x[k+11],S23,0x265e5a51); b=GG(b,c,d,a,x[k+0],S24,0xe9b6c7aa);
+    a=GG(a,b,c,d,x[k+5],S21,0xd62f105d); d=GG(d,a,b,c,x[k+10],S22,0x2441453);
+    c=GG(c,d,a,b,x[k+15],S23,0xd8a1e681); b=GG(b,c,d,a,x[k+4],S24,0xe7d3fbc8);
+    a=GG(a,b,c,d,x[k+9],S21,0x21e1cde6); d=GG(d,a,b,c,x[k+14],S22,0xc33707d6);
+    c=GG(c,d,a,b,x[k+3],S23,0xf4d50d87); b=GG(b,c,d,a,x[k+8],S24,0x455a14ed);
+    a=GG(a,b,c,d,x[k+13],S21,0xa9e3e905); d=GG(d,a,b,c,x[k+2],S22,0xfcefa3f8);
+    c=GG(c,d,a,b,x[k+7],S23,0x676f02d9); b=GG(b,c,d,a,x[k+12],S24,0x8d2a4c8a);
+
+    a=HH(a,b,c,d,x[k+5],S31,0xfffa3942); d=HH(d,a,b,c,x[k+8],S32,0x8771f681);
+    c=HH(c,d,a,b,x[k+11],S33,0x6d9d6122); b=HH(b,c,d,a,x[k+14],S34,0xfde5380c);
+    a=HH(a,b,c,d,x[k+1],S31,0xa4beea44); d=HH(d,a,b,c,x[k+4],S32,0x4bdecfa9);
+    c=HH(c,d,a,b,x[k+7],S33,0xf6bb4b60); b=HH(b,c,d,a,x[k+10],S34,0xbebfbc70);
+    a=HH(a,b,c,d,x[k+13],S31,0x289b7ec6); d=HH(d,a,b,c,x[k+0],S32,0xeaa127fa);
+    c=HH(c,d,a,b,x[k+3],S33,0xd4ef3085); b=HH(b,c,d,a,x[k+6],S34,0x4881d05);
+    a=HH(a,b,c,d,x[k+9],S31,0xd9d4d039); d=HH(d,a,b,c,x[k+12],S32,0xe6db99e5);
+    c=HH(c,d,a,b,x[k+15],S33,0x1fa27cf8); b=HH(b,c,d,a,x[k+2],S34,0xc4ac5665);
+
+    a=II(a,b,c,d,x[k+0],S41,0xf4292244); d=II(d,a,b,c,x[k+7],S42,0x432aff97);
+    c=II(c,d,a,b,x[k+14],S43,0xab9423a7); b=II(b,c,d,a,x[k+5],S44,0xfc93a039);
+    a=II(a,b,c,d,x[k+12],S41,0x655b59c3); d=II(d,a,b,c,x[k+3],S42,0x8f0ccc92);
+    c=II(c,d,a,b,x[k+10],S43,0xffeff47d); b=II(b,c,d,a,x[k+1],S44,0x85845dd1);
+    a=II(a,b,c,d,x[k+8],S41,0x6fa87e4f); d=II(d,a,b,c,x[k+15],S42,0xfe2ce6e0);
+    c=II(c,d,a,b,x[k+6],S43,0xa3014314); b=II(b,c,d,a,x[k+13],S44,0x4e0811a1);
+    a=II(a,b,c,d,x[k+4],S41,0xf7537e82); d=II(d,a,b,c,x[k+11],S42,0xbd3af235);
+    c=II(c,d,a,b,x[k+2],S43,0x2ad7d2bb); b=II(b,c,d,a,x[k+9],S44,0xeb86d391);
+
+    a=addUnsigned(a,AA); b=addUnsigned(b,BB); c=addUnsigned(c,CC); d=addUnsigned(d,DD);
+  }
+
+  return (toHex(a) + toHex(b) + toHex(c) + toHex(d)).toLowerCase();
+}
